87f7f35747b95531fb6613bda7c602497c6cf709
Zh0ny <caio.philipe.queiroz@gmail.com>
2025-10-18 08:28:02 -0300

    Lesson 4 : Add initial HTML, CSS, and trophy image for JS game project
    - Created index.html with basic structure and game content
    - Added style.css for styling the game interface
    - Included trophy.png image to enhance visual feedback for users

---
+++ logica-js-projeto_inicial_Aula_4/app.js
@@ -0,0 +1,24 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numeroSecreto = parseInt(Math.random() * 31);
+let tentativas = 1;
+
+while (palpite !== numeroSecreto) {
+    console.log(`Número secreto: ${numeroSecreto}, Tentativas restantes: ${tentativas}`)  // linha para ajudar nos testes
+    let palpite = parseInt(prompt("Digite um número entre 0 e 30:"));
+    if (palpite === numeroSecreto) {
+        alert("Parabéns! Você acertou o número secreto.");
+        break;
+    } else {
+        tentativas--;
+        if (palpite < numeroSecreto) {
+            alert(`Você errou! O número é maior. Quantidade de tentativas: ${tentativas}`);
+        } else {
+            alert(`Você errou! O número é menor. Quantidade de tentativas: ${tentativas}`);
+        }
+        tentativas++;
+    }
+    if (tentativas === 100) {
+        alert(`Suas tentativas acabaram! O número secreto era: ${numeroSecreto} :(`);
+        break;
+    }   
+}
